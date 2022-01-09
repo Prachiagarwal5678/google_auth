02f8662e9ccd7082dd1ed4875c8ce29ce75b7819
prachiagarwal5678 <prachiagarwal5678@gmail.com>
2022-01-09 18:16:56 +0530

    google auth

---
+++ app.js
@@ -0,0 +1,152 @@
+//jshint esversion:6
+// require(dotenv).config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const session=require("express-session");//step 1..
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");//step 1 end
+const GoogleStrategy = require('passport-google-oauth20').Strategy;//STEP 1
+const findOrCreate=require("mongoose-findorcreate");//STEP3
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
+
+const app = express();
+
+// console.log(process.env.API_KEY);
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//step2:set up session
+app.use(session({
+secret:"Our Little Secret.",
+resave: false,
+saveUninitialized: false
+}));
+
+app.use(passport.initialize()); //step 3
+app.use(passport.session());//step 4
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+
+//the schema needs to be mongoose schema inorder to have plugin .It cant be normal schema
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String,
+  googleId:String
+});
+
+userSchema.plugin(passportLocalMongoose);//step 5
+userSchema.plugin(findOrCreate);//STEP4
+
+const User=new mongoose.model("User",userSchema);
+
+//step 6..
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(function(user, done) {
+done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+User.findById(id, function(err, user) {
+  done(err, user);
+});
+});
+
+//step 6 ends
+//STEP2..
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets/",
+    // userProfileURL: "https://www.googleapis.com/oauth20/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));//STEP2 ends
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render ("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.get("/auth/google",
+  passport.authenticate('google',{scope:["profile"]})
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+
+    res.redirect("/secrets");
+  });
+
+app.post("/register",function(req,res){
+User.register({username:req.body.username},req.body.password,function(err,user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  }
+  else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+app.post("/login",function(req,res){
+
+  const user=new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  })
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
